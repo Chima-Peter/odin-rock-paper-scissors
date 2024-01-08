83318a480b9be5b2978b8f0561a408bba622d21b
OJIMMA CHIMA PETER <chimaojimma@gmail.com>
2024-01-08 20:00:41 +0100

    NEW PROJECT - ROCK PAPER SCISSORS

--- index.js
+++ index.js
@@ -1,57 +1,129 @@
+function displayResult(arg) {
+    const displayDiv = document.querySelector('#displayDiv');
+    const midDiv = document.createElement('div');
+    const tempPlayer = arg.cloneNode(true);
+    const tempComputer = getComputerChoice();
+
+    setUpPlayGame(tempPlayer, tempComputer)
+    midBtn.textContent = 'PLAY AGAIN';
+    midDiv.appendChild(midPara);
+    midDiv.appendChild(midBtn);
+    displayDiv.appendChild(tempPlayer);
+    displayDiv.appendChild(midDiv);
+    displayDiv.appendChild(tempComputer);
+}
+
+function setUpPlayGame(arg1, arg2) {
+    let playChoice;
+    let computerChoice;
+    if (arg1.id === 'rock') {
+        playChoice = 'ROCK'
+    } else if (arg1.id === 'scissors') {
+        playChoice = 'SCISSORS';
+    } else {
+        playChoice = 'PAPER';
+    }
+    if (arg2.id === 'rock') {
+        computerChoice = 'ROCK'
+    } else if (arg2.id === 'scissors') {
+        computerChoice = 'SCISSORS';
+    } else {
+        computerChoice = 'PAPER';
+    }
+    playGame(playChoice, computerChoice);
+}
+
 // function to get the computer's choice
 function getComputerChoice() {
     let computer = Math.floor(Math.random() * 3);
     if (computer === 0) {
-        return "ROCK";
+        return rock;
     } else if (computer === 1) {
-        return "PAPER";
+        return paper;
     } else {
-        return "SCISSORS";
+        return scissors;
     }
 }
+
 
 // fucntion to play the actual rock paper scissors game
 function playGame(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        para.textContent = "You drew a tie with computer. Replay";
+        midPara.textContent = "YOU DREW";
     }
     else if (playerSelection === 'ROCK') {
         if (computerSelection === 'SCISSORS') {
-            para.textContent = 'You win, Rock beats Scissors';
+            midPara.textContent = 'YOU WIN';
         } else {
-            para.textContent = 'You lose, Paper beats Rock';
+            midPara.textContent = 'YOU LOSE';
         }
     } else if (playerSelection === 'SCISSORS') {
         if (computerSelection === 'ROCK') {
-            para.textContent = 'You lose, Rock beats Scissors';
+            midPara.textContent = 'YOU LOSE';
         }
         else {
-            para.textContent = 'You win, Scissors beats Paper';
+            midPara.textContent = 'YOU WIN';
         }
     } else if (playerSelection === 'PAPER') {
         if (computerSelection === 'ROCK') {
-            para.textContent = 'You win, Paper beats Rock';
+            midPara.textContent = 'YOU WIN';
         }
         else {
-            para.textContent = 'You lose, Scissors beats Paper';
+            midPara.textContent = 'YOU LOSE';
         }
     }
 }
-let container = document.querySelector("#container");
-let div = document.createElement('div');
-let para = document.createElement('p');
-container.addEventListener('click', (event) => {
-    switch(event.target.id) {
-        case 'rock':
-            playGame('ROCK', 'SCISSORS');
-            break;
-        case 'paper':
-            playGame('PAPER', getComputerChoice());
-            break;
-        case 'scissors':
-            playGame('SCISSORS', getComputerChoice());
-            break;
-    }
-    div.appendChild(para);
-    container.appendChild(div);
-})
+
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const body = document.querySelector('body');
+const footer = document.querySelector('#footer');
+const rpsContainer = document.querySelector('#img-container');
+const ruleParent = document.querySelector('#rule-div');
+const ruleBtn = document.querySelector('#btn-div');
+const rpDiv = document.createElement('div');
+const holdDiv = document.querySelector('#holder');
+const playerParagraph = document.createElement('p');
+const computerParagraph = document.createElement('p');
+const midPara = document.createElement('p');
+const midBtn = document.createElement('button');
+playerParagraph.textContent = 'YOU PICKED';
+computerParagraph.textContent = 'THE HOUSE PICKED';
+rpDiv.appendChild(playerParagraph);
+rpDiv.appendChild(computerParagraph);
+
+
+rock.addEventListener('click', () => {
+    body.removeChild(rpsContainer);
+    ruleParent.removeChild(ruleBtn);
+    holdDiv.appendChild(rpDiv);
+    body.removeChild(footer);
+    rpDiv.classList.add('rpDiv');
+    displayResult(rock);
+});
+
+
+paper.addEventListener('click', () => {
+    body.removeChild(rpsContainer);
+    ruleParent.removeChild(ruleBtn);
+    holdDiv.appendChild(rpDiv);
+    body.removeChild(footer);
+    rpDiv.classList.add('rpDiv');
+    displayResult(paper);
+});
+
+
+scissors.addEventListener('click', () => {
+    body.removeChild(rpsContainer);
+    ruleParent.removeChild(ruleBtn);
+    holdDiv.appendChild(rpDiv);
+    body.removeChild(footer);
+    rpDiv.classList.add('rpDiv');
+    displayResult(scissors);
+});
+
+midBtn.addEventListener('click', () => {
+    window.location.reload();
+});
